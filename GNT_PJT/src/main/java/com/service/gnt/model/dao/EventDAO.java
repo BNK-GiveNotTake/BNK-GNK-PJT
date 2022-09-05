c770f1f28aead23c5ebf5df66d317082b7248e58
Jaeho Jeong <jaho901@naver.com>
2022-09-05 12:27:16 +0900

    feat: update Event

--- GNT_PJT/src/main/java/com/service/gnt/model/dao/EventDAO.java
+++ GNT_PJT/src/main/java/com/service/gnt/model/dao/EventDAO.java
@@ -5,19 +5,20 @@ import com.service.gnt.domain.account.MileageHistory;
 import com.service.gnt.domain.event.Quiz;
 public interface EventDAO {
 	// 퀴즈 풀이 여부
-	String checkQuizPlayed(String userId) throws Exception;
+	String checkQuizPlayed(int userId) throws Exception;
 	// 퀴즈 데이터 가져오기
-	String selectQuizId(String userId) throws Exception;
-	Quiz getQuiz(String quizId) throws Exception;
+	int selectQuizId(int userId) throws Exception;
+	Quiz getQuiz(int quizId) throws Exception;
 	// 퀴즈 정답 반환 및 고객 퀴즈 데이터 변경
-	int getQuizAnswer(String quizId) throws Exception;
-	int updateQuizUser(String userId) throws Exception;
+	int getQuizAnswer(int quizId) throws Exception;
+	int updateQuizUser(int userId) throws Exception;
 	// 고객이 정답을 맞췄을 시
-	String selectQuizAccId(String userId) throws Exception;
+	String selectQuizAccId(int userId) throws Exception;
 	int updateQuizMileage(Account account) throws Exception;
 	int selectMilieageHistorySeq() throws Exception;
 	int insertMilieage(MileageHistory mh) throws Exception;
 	// 퀴즈와 룰렛 세팅
-	int updateQuizReset() throws Exception;
+	int updateEventReset() throws Exception;
+	int updateRouletteUser(int userId) throws Exception;
 	List<Integer> selectAllUserId() throws Exception;
 }
