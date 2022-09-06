4624f915d7cc0d391718135aea5aaefd8a1d03ed
Jaeho Jeong <jaho901@naver.com>
2022-09-06 10:40:13 +0900

    feat: update event game

--- GNT_PJT/src/main/java/com/service/gnt/model/service/EventServiceImpl.java
+++ GNT_PJT/src/main/java/com/service/gnt/model/service/EventServiceImpl.java
@@ -1,9 +1,12 @@
 package com.service.gnt.model.service;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.service.gnt.domain.account.Account;
 import com.service.gnt.domain.account.MileageHistory;
+import com.service.gnt.domain.event.Game;
 import com.service.gnt.domain.event.Quiz;
 import com.service.gnt.model.dao.EventDAO;
 @Service
@@ -53,12 +56,65 @@ public class EventServiceImpl implements EventService{
 	// 퀴즈 리셋
 	public void resetEvent() throws Exception {
 		eventDAO.updateEventReset();
+		eventDAO.updateGameIsTodayReset(); // 애플리케이션 구동할 때랑... 0시에 리셋돼도 괜찮은 건가?
 	}
 	// 돌림판 참가자 반환
 	public List<Integer> resetRoulette() throws Exception {
 		List<Integer> list = eventDAO.selectAllUserId();
 		return list;
 	}
+	
+	
+	// 2020.09.05 추가
+	
+	// 게임 참가 여부 (오늘 - no.3 눌렀을 때)
+	public String checkGamePlayed(int userId) throws Exception {
+		return eventDAO.checkGamePlayed(userId);
+	}
+	// 게임 처음 시작할 때 (게임 테이블 생성 - 카드 안 만들면 못 함)
+	public void insertGame(Game game) throws Exception {
+		int gameId = eventDAO.selectGameSeq();
+		game.setGameId(gameId);
+		eventDAO.insertGame(game);
+	}
+	// 저장된 게임 데이터 호출
+	public Game getGame(int userId) throws Exception {
+		return eventDAO.getGame(userId);
+	}
+	// 게임 실행 후
+	public void updateGame(Game game) throws Exception {
+		eventDAO.updateGame(game);
+	}
+	
+	public void updateGameForMilieage(int userId, int mp) throws Exception {
+		String accId = eventDAO.selectQuizAccId(userId); // 계좌 번호
+		Account account = new Account(); // 계좌 VO에 계좌번호, 마일리지 금액 생성
+		account.setAccId(accId);
+		account.setMileage(mp);
+		eventDAO.updateQuizMileage(account); // 계좌 속 마일리지 변경
+		int mhSeq = eventDAO.selectMilieageHistorySeq(); // 마일리지 내역 seq
+		MileageHistory mh = new MileageHistory(); // 마일리지 내역 VO에 값 할당 
+		mh.setMileagePk(mhSeq);
+		mh.setAccId(accId);
+		mh.setMileageAmount(mp);
+		mh.setMileageContent("게임");
+		eventDAO.insertMilieage(mh); // 마일리지 내역 생성
+	}
+	// 0시 0분에, tomorrow가 0인 사람은 이걸 해야 해
+	public void resetEventCheckTomorrow() throws Exception {
+		List<Game> list = eventDAO.checkGamePlayedTomorrow();
+		for (Game game : list) {
+			if (game.getIsTomorrow()=='1') {
+				eventDAO.updateGameIsTomorrowReset(game.getUserId());
+			}
+		}
+	}
+	@Override
+	public void updateGameForMoney(int userId, int money) throws Exception {
+		// 계좌에 돈 넣어주기
+		
+	}
+
 
 
 
